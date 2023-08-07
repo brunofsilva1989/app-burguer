162b4ceefa8da07471b0d06c41bd9108a2b5fb91
Bruno F Silva <78174347+brunofsilva1989@users.noreply.github.com>
2023-08-06 22:35:26 -0300

    Mudanças feitas no carrinho, precisa corrigir.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
 
 var cardapio = {};
 
+var MEU_CARRINHO = [];
+
 cardapio.eventos = {
     init: () => {
         cardapio.metodos.obterItensCardapio();
@@ -13,19 +15,35 @@ cardapio.eventos = {
 cardapio.metodos = {
 
     //Obtem a lista de itens do cardápio
-    obterItensCardapio: (categoria = 'burgers') => {
+    obterItensCardapio: (categoria = 'burgers', vermais = false) => {
 
         var filtro = MENU[categoria];
         console.log(filtro);
 
-        $("#itensCardapio").html('')
+        if (!vermais) {
+            $("#itensCardapio").html('')
+            $("#btnVerMais").removeClass('hidden');
+        }
+
 
         $.each(filtro, (i, e) => {
-        
-            let temp = cardapio.templates.item.replace(/\${img}/g,e.img).replace(/\${name}/g,e.name).replace(/\${price}/g,e.price.toFixed(2).replace('.',','));
 
-            $("#itensCardapio").append(temp)
-            
+            let temp = cardapio.templates.item.replace(/\${img}/g, e.img)
+                .replace(/\${name}/g, e.name)
+                .replace(/\${price}/g, e.price.toFixed(2)
+                    .replace('.', ','))
+                    .replace(/\${id}/g, e.id);
+
+            //Botao ver mais foi clicado
+            if (vermais && i >= 8 && i < 12) {
+                $("#itensCardapio").append(temp)
+            }
+
+            //Paginação Inicial (8 itens)
+            if (!vermais && i < 8) {
+                $("#itensCardapio").append(temp)
+            }
+
         })
 
         //remove o active do botao burgers
@@ -33,31 +51,131 @@ cardapio.metodos = {
 
         // seta o menu para ativo para a categoria selecionada
         $("#menu-" + categoria).addClass('active')
+    },
+
+    //Clique no botão de vermais
+    verMais: () => {
+
+        var ativo = $(".container-menu a.active").attr('id').split('menu-')[1]; //menu-burgers exemplo
+
+        cardapio.metodos.obterItensCardapio(ativo, true);
+
+        $("#btnVerMais").addClass('hidden');
+
+    },
+
+    //diminuir a quantidade de item no cardápio
+    diminuirQuantidade: (id) => {
+
+        let qntdAtual = parseInt($("#qntd-" + id).text());
+        if (qntdAtual > 0) {
+            $("#qntd-" + id).text(qntdAtual - 1)
+        }
+
+    },
+
+    //aumentar a quantidade de item no cardapio
+    aumentarQuantidade: (id) => {
+
+        let qntdAtual = parseInt($("#qntd-" + id).text());
+        $("#qntd-" + id).text(qntdAtual + 1)
+    },
+
+    //Adicionar ao carrinho o item do cardápio
+    adicionarAoCarrinho: (id) => {
+
+        let qntdAtual = parseInt($("#qntd-" + id).text());
+
+        if(qntdAtual > 0) {
+
+            //obter a categoria ativa
+            var categoria =  $(".container-menu a.active").attr('id').split('menu-')[1];
+
+            //Obter lista de itens
+            let filtro = MENU[categoria];
+
+            //Oter o item
+            let item = $.grep(filtro, (e, i) => { return e.id == id });
+
+            if(item.length > 0){
+
+                // valida se já existe esse item no carrinho
+                let existe = $.grep(MEU_CARRINHO, (elem, index) => { return elem.id == id });
+
+                // caso ja exista o item no carrinho, só altera a quantidade
+                if(existe.length > 0){
+
+                    let objIndex = MEU_CARRINHO.findIndex((obj => obj.id == id))
+                    MEU_CARRINHO[objIndex].qntd = MEU_CARRINHO[objIndex].qntd + qntdAtual;
+
+                }//Caso ainda não exista item no carrinho, adiciona ele.
+                else{
+                    item[0].qntd = qntdAtual;
+                    MEU_CARRINHO.push(item[0]); 
+                }
+
+                alert('Item adicionado ao carrinho')
+                $("#qntd-" + id).text(0);
+
+                cardapio.metodos.atualizarBagdeTotal();
+
+            }
+
+        }
+
+    },
+
+    // Atualiza badge de totais dos botoes MEU CARRINHO
+    atualizarBagdeTotal: () => {
+
+        var total = 0;
+
+        $.each(MEU_CARRINHO, (i, e) => {
+
+            total += e.qntd
+
+        })
+
+        if(total > 0){
+            $(".botao-carrinho").removeClass('hidden');
+            $(".container-total-carrinho").removeClass('hidden');
+            
+
+        }else{
+            $(".botao-carrinho").addClass('hidden');
+            $(".container-total-carrinho").addClass('hidden');
+        }
+
+        $(".badge-total-carrinho").html(total);
+
     }
+
+
 }
 
 cardapio.templates = {
 
-    item: `
-    <div class="col-3 mb-5">
-    <div class="card card-item">
-        <div class="img-produto">
-            <img
-                src="\${img}">
-        </div>
-        <p class="title-produto text-center mt-4">
-            <b>\${name}</b>
-        </p>
-        <p class="price-produto text-center">
-            <b>R$ \${price}</b>
-        </p>
-        <div class="add-carrinho">
-            <span class="btn-menos"><i class="fas fa-minus"></i></span>
-            <span class="add-numero-itens">0</span>
-            <span class="btn-mais"><i class="fas fa-plus"></i></span>
-            <span class="btn btn-add"><i class="fas fa-shopping-bag"></i></span>
+    item: ` <div class="col-3 mb-5">
+        <div class="card card-item" id="\${id}">
+            <div class="img-produto">
+                <img src="\${img}">
+            </div>
+            <p class="title-produto text-center mt-4">
+                <b>\${name}</b>
+            </p>
+            <p class="price-produto text-center">
+                <b>R$ \${price}</b>
+            </p>
+            <div class="add-carrinho">
+                <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidade('\${id}')"><i
+                        class="fas fa-minus"></i></span>
+                <span class="add-numero-itens" id="qntd-\${id}">0</span>
+                <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidade('\${id}')"><i
+                        class="fas fa-plus"></i></span>
+                <span class="btn btn-add" onclick="cardapio.metodos.adicionarAoCarrinho('\${id}')"><i class="fas fa-shopping-bag"></i></span>
+            </div>
         </div>
     </div>
-</div>`
-
-}
+    
+    `
+    }
