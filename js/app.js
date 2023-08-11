000497430b24ad54a352204444a5f4690205f99a
Bruno F Silva <78174347+brunofsilva1989@users.noreply.github.com>
2023-08-10 23:09:43 -0300

    Manipulação dos itens do Carrinho

--- js/app.js
+++ js/app.js
@@ -150,11 +150,12 @@ cardapio.metodos = {
 
     },
 
+    // Abrir modal carrinho
     abrirCarrinho: (abrir) => {
 
         if(abrir){
             $("#modalCarrinho").removeClass('hidden');
-            cardapio.metodos.carregarEtapa(1);
+            cardapio.metodos.carregarCarrinho();
         }else{
             $("#modalCarrinho").addClass('hidden');
         }
@@ -223,6 +224,80 @@ cardapio.metodos = {
 
     },
 
+    //carrega a lista de itens do carrinho
+    carregarCarrinho: () => {
+
+        cardapio.metodos.carregarEtapa(1);
+
+        if(MEU_CARRINHO.length > 0){
+            
+            $("#itensCarrinho").html('');
+
+            $.each(MEU_CARRINHO, (i, e) => {
+                let temp = cardapio.templates.itemCarrinho.replace(/\${img}/g, e.img)
+                .replace(/\${name}/g, e.name)
+                .replace(/\${price}/g, e.price.toFixed(2)
+                .replace('.', ','))
+                .replace(/\${id}/g, e.id)
+                .replace(/\${qntd}/g, e.qntd);   
+                
+                $("#itensCarrinho").append(temp);
+            })
+
+        }else{
+
+            $("#itensCarrinho").html('<p class="carrinho-vazio"><i class="fa fa-shopping-bag"></i>Seu carrinho está vazio.</p>');
+        }
+
+    },
+
+    //diminuir quantidade do item no carrinho
+    diminuirQuantidadeCarrinho: (id) => {
+
+        let qntdAtual = parseInt($("#qntd-carrinho-" + id).text());
+
+        if (qntdAtual > 1) {
+            $("#qntd-carrinho-" + id).text(qntdAtual - 1)
+            cardapio.metodos.atualizarCarrinho(id, qntdAtual - 1)
+        }
+        else{
+            cardapio.metodos.removerItemCarrinho(id);
+
+
+        }
+
+    },
+
+    //aumentar quantidade do item no carrinho
+    aumentarQuantidadeCarrinho: (id) => {
+
+        let qntdAtual = parseInt($("#qntd-carrinho-" + id).text());
+        $("#qntd-carrinho-" + id).text(qntdAtual + 1)
+        cardapio.metodos.atualizarCarrinho(id, qntdAtual + 1);
+
+    },
+
+    //botão remove o item do carrinho
+    removerItemCarrinho: (id) => {
+
+        MEU_CARRINHO = $.grep(MEU_CARRINHO, (e, i) => {return e.id != id});
+        cardapio.metodos.carregarCarrinho();
+
+        //atualiza o botão carrinho com a quantidade atualizada
+        cardapio.metodos.atualizarBagdeTotal();
+
+    },
+
+    //atualiza o carrinho com a quantidade atual
+    atualizarCarrinho: (id, qntd) => {
+
+        let objIndex = MEU_CARRINHO.findIndex((obj => obj.id == id));
+        MEU_CARRINHO[objIndex].qntd = qntd;
+
+        //Atualiza o botão carrinho com a quantidade atualizada
+        cardapio.metodos.atualizarBagdeTotal();
+
+    },
 
     // Cria uma mensagem após inserir um item no carrinho
     mensagem: (texto, cor = 'red', tempo = 3500) => {
@@ -244,7 +319,6 @@ cardapio.metodos = {
         }, tempo)
 
     }
-
 
 }
 
@@ -272,5 +346,27 @@ cardapio.templates = {
         </div>
     </div>
     
+    `,
+    itemCarrinho: ` 
+    <div class="col-12 item-carrinho">
+
+        <div class="img-produto">
+             <img src="\${img}"
+             alt="">
+        </div>
+        <div class="dados-produto">.
+            <p class="title-produto"><b>\${name}</b></p>
+            <p class="price-produto"><b>R$ \${price}</b></p>
+        </div>
+        <div class="add-carrinho">
+            <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidadeCarrinho('\${id}')"><i
+            class="fas fa-minus"></i></span>
+            <span class="add-numero-itens" id="qntd-carrinho-\${id}">\${qntd}</span>
+            <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidadeCarrinho('\${id}')"><i
+            class="fas fa-plus"></i></span>
+            <span class="btn btn-remove" onclick="cardapio.metodos.removerItemCarrinho('\${id}')"><i class="fas fa-times"></i></span>
+        </div>
+    </div>
     `
     }
+    
